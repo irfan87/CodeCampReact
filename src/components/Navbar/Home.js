0456e69cc25231e78069c17fca68d39a179b9e44
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2018-09-07 02:08:12 +0800

    Put images and set the image and text in responsive mode

--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -3,7 +3,16 @@ import React from "react";
 const Home = props => {
   return (
     <div>
-      <h2>Home stateless component</h2>
+      <div id="image-div">
+        <img
+          src="https://uploads.codesandbox.io/uploads/user/7ec48db7-47e5-4b6a-9501-053401b5d1bc/uLG8-codes.jpg"
+          alt="Codes"
+          id="codes_image"
+        />
+        <div id="codes-text-center">
+          <h3>Learn To Code Until You Get The Highest Confidence</h3>
+        </div>
+      </div>
     </div>
   );
 };
